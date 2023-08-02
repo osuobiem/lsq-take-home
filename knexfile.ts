ff8423be02e2615c3c3cf15c08be47b23e147cdc
Gabriel Osuobiem <osuobiem@gmail.com>
2023-08-02 01:15:21 +0100

    configure knex

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+import type {Knex} from "knex";
+
+const knexConfig: {[key: string]: Knex.Config} = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+export default knexConfig;
